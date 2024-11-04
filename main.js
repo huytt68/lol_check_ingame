4ffc9ba70ed5e6302f708f9328c155627ddba1b8
huytt68 <trantronghuy68@gmail.com>
2024-11-04 23:33:10 +0700

    fix: send 1 message/1 game & noti game ended

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const axios = require('axios');
-require('dotenv').config();
 const moment = require('moment');
 const clc = require('cli-color');
+const cron = require('node-cron');
+
+require('dotenv').config();
 
 const RIOT_API_KEY = process.env.RIOT_API_KEY;
 const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
 
 // Thay thế PUUID đơn lẻ bằng mảng các PUUID
-const accounts = [
+const PUUIDS = [
 	'kDCBiPhDSjRRcynFvWvIDJ0DcDOibp96iVs0p_YHyQMuiXie2cRIpsZQSn5ugzXldIVJRIT3OoByBw', //Canh
 	'A8EPhv7ChKwa3N12oTC2zZ6R-RFe0l3QSC4IaR09ccp6l7leiHuq7vM3cFEXui8zUICX6xC6NIAYvQ', //Acc 2
 	'5_rMZmg-6iXhmXyt6KDkvlG3fLFBi_cXQzrT6wjTINsDOXttKjGFTLqZ1MCis4Y9hKLeSHTip7BhVQ', //Thanh
@@ -18,9 +20,19 @@ const accounts = [
 	'Zfl6WezAOAyt0BEjAD2ThV25eJr1MIGHkG2OHfj7fPuxvoEzTyZna2FaPSBct-YKEAY6fV4G8vYL_w', //Vinh
 	'SEmrr51edIxdP0jKYh06wSG5oT1tTBs8Gu-YRWvLVVBDNANEd4Hn5OE_Q4wnqpvdkJZkAYuiCDF6Gw', //Long
 	'-W501c8FUJByT6vwR4NcUBm0SdEVMMqLo84pS7Vqb-Ydh3y4gucm7FvD4ZLaRX2mozEi3Ra7R4pOUg', //Manh
+	'vlJzxNCdhjYDvadtKmAXh-O9-4N5IE-KP6ItgkPkHAffnGb6NP17loR-b5VJHgjejtqlZCArJC_PpQ', //cutu
 ];
-let img_url = '';
-let color = '';
+let activeMatches = {};
+
+async function sendDiscordMessage(data) {
+	try {
+		await axios.post(DISCORD_WEBHOOK_URL, data);
+		// console.log('Đã gửi thông báo đến Discord:', data);
+	} catch (error) {
+		console.error('Lỗi khi gửi thông báo Discord:', error.message);
+	}
+}
+
 const setImgAndColorByMode = (gameMode) => {
 	if (gameMode == 'ULTBOOK') {
 		img_url =
@@ -35,6 +47,8 @@ const setImgAndColorByMode = (gameMode) => {
 			'https://static.wikia.nocookie.net/leagueoflegends/images/0/07/Howling_Abyss_Minimap.png';
 		color = '31743';
 	}
+
+	return { img_url: img_url, color: color };
 };
 
 // Sử dụng webhook riêng cho từng account
@@ -58,6 +72,7 @@ async function checkActiveGame(puuid) {
 		const timestamp = new Date().toISOString();
 		if (response.status == 200) {
 			const gameInfo = response.data;
+
 			// get list players
 			const players = gameInfo.participants;
 			const players_blue = players.slice(0, 5).reduce((string, p) => string + p.riotId + '\n', '');
@@ -70,15 +85,14 @@ async function checkActiveGame(puuid) {
 			const gameLenFormatted = `${gameLenMinutes.toString().padStart(2, '0')}:${gameLenSeconds
 				.toString()
 				.padStart(2, '0')}`;
-			const startTime = moment(gameInfo.gameStartTime).format('MMM d YYYY, HH:mm:ss');
+			const startTime = moment(gameInfo.gameStartTime).format('YYYY-MM-DD HH:mm:ss');
 
-			setImgAndColorByMode(gameInfo.gameMode);
-
+			const { img_url, color } = setImgAndColorByMode(gameInfo.gameMode);
 			const data = {
 				embeds: [
 					{
-						title: `${riotId} - in game #${gameInfo.gameId}`,
-						description: `Mode: ${gameInfo.gameMode}`,
+						title: `${riotId}`,
+						description: `Mode: ${gameInfo.gameMode}\nGameID: ${gameInfo.gameId}`,
 						color: color,
 						footer: {
 							text: `${startTime} - Ingame: ${gameLenFormatted}`,
@@ -91,7 +105,6 @@ async function checkActiveGame(puuid) {
 							{ name: 'Team 1:', value: players_blue, inline: true },
 							{ name: 'Team 2:', value: players_red, inline: true },
 						],
-						// image: { url: img_url },
 						timestamp: timestamp,
 						thumbnail: {
 							url: img_url,
@@ -100,9 +113,21 @@ async function checkActiveGame(puuid) {
 				],
 				content: '',
 			};
-			await axios.post(DISCORD_WEBHOOK_URL, data);
-			const now = moment().format('MMM d YYYY, HH:mm:ss');
 
+			// match id
+			const matchId = gameInfo.gameId;
+			if (matchId) {
+				// if new match
+				if (!activeMatches[matchId]) {
+					activeMatches[matchId] = new Set();
+					await sendDiscordMessage(data);
+				}
+				// add player to match
+				activeMatches[matchId].add(puuid);
+			}
+
+			// Log
+			const now = moment().format('MMM d YYYY, HH:mm:ss');
 			console.log(clc.green(`${now} --- ${riotId} in game`));
 		}
 	} catch (error) {
@@ -114,25 +139,35 @@ async function checkActiveGame(puuid) {
 		);
 		const userInfo = user_response.data;
 		const riotId = `${userInfo.gameName}#${userInfo.tagLine}`;
-		const timestamp = moment().format('MMM d YYYY, HH:mm:ss');
 		if (error.response && error.response.status == 404) {
-			console.log(`${timestamp} --- ${riotId} not in game`);
+			for (let matchId in activeMatches) {
+				if (activeMatches[matchId].has(puuid)) {
+					activeMatches[matchId].delete(puuid);
+					// Nếu không còn ai trong trận, xóa luôn matchId khỏi activeMatches
+					if (activeMatches[matchId].size === 0) {
+						await sendDiscordMessage({ content: `Trận đấu #${matchId} đã kết thúc!` });
+						delete activeMatches[matchId];
+					}
+				}
+			}
 		} else {
-			console.error(clc.red(`${timestamp} --- Error for ${riotId}:`, error.message));
+			console.error(clc.red(`${now} --- Error for ${riotId}:`, error.message));
 		}
 	}
 }
 
-// Sửa phần chạy chương trình
-function checkAllAccounts() {
-	accounts.forEach((puuid, index) => {
+// Check all
+const checkAllAccounts = () => {
+	PUUIDS.forEach((puuid, index) => {
 		setTimeout(async () => {
 			await checkActiveGame(puuid);
 		}, index * 1000);
 	});
-}
+};
 
-// Chạy lần đầu
-checkAllAccounts();
-// Lập lịch kiểm tra mỗi 5 phút
-setInterval(checkAllAccounts, 5 * 60 * 1000);
+//  Lập lịch kiểm tra mỗi 5 phút
+cron.schedule('*/1 * * * *', () => {
+	const now = moment().format('MMM d YYYY, HH:mm:ss');
+	console.log(`~~~~~~~~~~~ ${now} ~~~~~~~~~~`);
+	checkAllAccounts();
+});
